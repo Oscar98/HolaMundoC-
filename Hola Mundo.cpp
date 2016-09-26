5d0395494bb7630f221536e61eb6e46a53ab8d95
Oscar Eduardo Martinez Gonzalez <oscar_1698@hotmail.com>
2016-09-26 02:06:34 -0500

    Hola Mundo C++
    Primer programa echo en un entorno de programacion a c++

---
+++ Hola Mundo.cpp
@@ -0,0 +1,20 @@
+/* 
+	Programa: Hola Mundo
+	Autor: Oscar Martinez Gonzalez
+	Carrera: Ing. Informatica 2016B
+	Materia: Programacion 	Seccion: D24 
+*/	 
+
+//Definir las librerias que vamos a utilizar en nuestro programa 
+#include <iostream> 
+
+/*definir el uso de epacios para no declerarlo en cada linea de nuestro codigo*/
+using namespace std;
+
+int main(){
+	//Mensaje que se va mostra en pantalla
+	cout<<"Hola Mundo"<<endl;
+	//instruccion para finalizar y salir del programa
+	return 0;
+} 
+
